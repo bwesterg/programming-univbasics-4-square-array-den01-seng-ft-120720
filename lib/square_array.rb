967f084c326bea5898357a646dad633b19c1dcf4
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-19 19:16:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+
+               
+def square_arrray(1,2,3)
+  counter = 0 
+  
+  while counter <= square_array.length do
+    puts square_array ** 2 
+    
+  end
 end
